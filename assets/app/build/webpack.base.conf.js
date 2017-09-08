1b9cda3063f52172579af00d103bd78e0c4454c8
Jan <jan_rock@hotmail.de>
2017-09-08 22:24:25 +0200

    Adding SCSS support

--- assets/app/build/webpack.base.conf.js
+++ assets/app/build/webpack.base.conf.js
@@ -37,6 +37,16 @@ module.exports = {
         }
       },
       {
+        test: /\.scss$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }, {
+            loader: "sass-loader" // compiles Sass to CSS
+        }]
+      }, 
+      {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: vueLoaderConfig
