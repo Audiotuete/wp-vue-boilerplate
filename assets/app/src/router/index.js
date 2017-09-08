e99970d063c48c064f8dc987802e9732303b123e
Jan <jan_rock@hotmail.de>
2017-09-08 12:11:09 +0200

    Setting up Routes and Test Components

--- assets/app/src/router/index.js
+++ assets/app/src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Startseite from '@/components/Startseite'
+import Idee from '@/components/Idee'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
+    { path: '/idee', name: 'Idee', component: Idee },
+    { path: '*', name: 'Alles', component: Startseite }
   ]
 })
